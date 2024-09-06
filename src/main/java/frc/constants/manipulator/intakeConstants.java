199d7630f8d64267c4ba2d96679efb9b74a84f0b
Mason A Paprzycki <mason44.pap@gmail.com>
2024-09-05 20:36:30 -0600

    added subsystems

--- src/main/java/frc/constants/manipulator/intakeConstants.java
+++ src/main/java/frc/constants/manipulator/intakeConstants.java
@@ -1,13 +1,12 @@
 package frc.constants.manipulator;
 
 public abstract class intakeConstants {
-    
-    public static class RollerMotor {
-        public static final String NAME = "Roller Motor";
-        ////TODO: update the motor port value
-        public static final int MOTOR_PORT = -1;
-        public static final int CURRENT_LIMT = 40;
-        public static final boolean INVERTED = true;
-      }
 
+  public static class RollerMotor {
+    public static final String NAME = "Roller Motor";
+    //// TODO: update the motor port value
+    public static final int MOTOR_PORT = -1;
+    public static final int CURRENT_LIMT = 40;
+    public static final boolean INVERTED = true;
+  }
 }
---
+++ src/main/java/frc/robot/constants/manipulator/IntakeConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants.manipulator;
+
+public class IntakeConstants {
+
+  public static class IntakeMotor {
+    public static final String NAME = "Intake Motor";
+    public static final int MOTOR_PORT = -1;
+    public static final int CURRENT_LIMT = 20;
+    public static final boolean INVERTED = true;
+  }
+}

a65fc6a500bba6b52a6a3261ad4657e3b1e552dc
Mason A Paprzycki <mason44.pap@gmail.com>
2024-09-05 20:58:38 -0600

    shooter subsystem changes

--- src/main/java/frc/robot/subsystems/manipulator/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipulator/ShooterSubsystem.java
@@ -1,5 +1,25 @@
 package frc.robot.subsystems.manipulator;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.bearbotics.motor.MotorPidBuilder;
+import frc.constants.manipulator.ShooterConstants;
 
-public class ShooterSubsystem extends SubsystemBase {}
+public class ShooterSubsystem extends SubsystemBase {
+  private final boolean SHUFFLEBOARD_ENABLED = true;
+
+  private CANSparkMax angleMotor;
+  private CANSparkMax shootingMotor;
+
+  private RelativeEncoder relativeAngleEncoder;
+
+  private double targetVelocity;
+
+  private void configureMotors() {
+    MotorPidBuilder shootingMotorPIDConfig =
+        new MotorPidBuilder()
+            .withP(ShooterConstants.ShootingMotor.MotorPid.P)
+            .withFf(ShooterConstants.ShootingMotor.MotorPid.Ff);
+  }
+}

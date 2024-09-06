169d3b71990ae68f95c852e4c1909cfda106e0ea
Kellen <70085049+Kellenn@users.noreply.github.com>
2024-09-05 20:50:19 -0600

    Add inital drivetrain code

---
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
+public class DriveConstants {
+  // Controller ports.
+  public static final int DRIVER_CONTROLLER_PORT = 0;
+
+  // YAGSL telemetry verbosity.
+  public static final TelemetryVerbosity TELEMETRY_VERBOSITY = TelemetryVerbosity.HIGH;
+}

a4f5bf2f2e0dbb36481e3751047313d1432c26ae
robertmakowsky <robmakowsky@icloud.com>
2024-09-09 17:49:15 -0600

    basic code for motor setup

--- src/main/java/frc/constants/manipulator/intakeConstants.java
+++ src/main/java/frc/constants/manipulator/intakeConstants.java
@@ -1,9 +1,9 @@
 package frc.constants.manipulator;
 
-public abstract class intakeConstants {
+public abstract class IntakeConstants {
 
-  public static class RollerMotor {
-    public static final String NAME = "Roller Motor";
+  public static class IntakeMotor {
+    public static final String NAME = "Intake Motor";
     //// TODO: update the motor port value
     public static final int MOTOR_PORT = -1;
     public static final int CURRENT_LIMT = 40;

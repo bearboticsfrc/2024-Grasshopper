5fd8d4ec7827d760bbc47610648cbd66fd441edc
Mason A Paprzycki <mason44.pap@gmail.com>
2024-09-10 11:54:16 -0600

    merged changes

--- src/main/java/frc/robot/constants/manipulator/IntakeConstants.java
+++ src/main/java/frc/robot/constants/manipulator/IntakeConstants.java
@@ -1,9 +1,13 @@
 package frc.robot.constants.manipulator;
 
-public class IntakeConstants {
+public abstract class IntakeConstants {
+
+  // TODO: Update INTAKE_BEAM_BREAK_CHANNEL
+  public static final int INTAKE_BEAM_BREAK_CHANNEL = -1;
 
   public static class IntakeMotor {
     public static final String NAME = "Intake Motor";
+    // TODO: Update Motor_PORT
     public static final int MOTOR_PORT = -1;
     public static final int CURRENT_LIMT = 20;
     public static final boolean INVERTED = true;
